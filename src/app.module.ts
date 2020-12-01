8fedd711f6320d685f0648921491ba81d947b67d
isaias luiz <isaiaslds63@gmail.com>
2020-12-01 01:45:13 -0300

    Personalizando README.md e removendo imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,6 @@ import { GraphQLModule } from '@nestjs/graphql';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { JobService } from './jobs/job/job.service';
-import { AppService } from './app.service';
-
-import { AppController } from './app.controller';
 
 import { JobResolver } from './jobs/job/job.resolver';
 
